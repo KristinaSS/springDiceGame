ca5f3aa90895e54e4b7f662d386a82b04ecf51b1
Kristina <miss.krisi@abv.bg>
2019-09-25 18:40:31 +0300

    Initial commit

---
+++ src/main/java/com/generala/springDiceGame/SpringDiceGameApplication.java
@@ -0,0 +1,13 @@
+package com.generala.springDiceGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiceGameApplication.class, args);
+	}
+
+}
